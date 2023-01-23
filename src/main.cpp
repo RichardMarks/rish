ecdd0994abd8b44b22a9b0ace729e3dcaaf5c190
Richard Marks <ccpsceo@gmail.com>
2023-01-22 19:16:36 -0600

    feat(window): sfml window creation and minimal main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(320, 240), "rish");
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+      else if (event.type == sf::Event::KeyPressed)
+      {
+        if (event.key.code == sf::Keyboard::Escape)
+        {
+          window.close();
+        }
+      }
+    }
+    window.clear();
+    window.display();
+  }
+
   return 0;
 }
